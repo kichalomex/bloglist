12a9c7c63c63a4feb4aaf7695339db4aff28fe38
kichalomex <kichalo23con@hotmail.com>
2023-05-02 22:37:17 -0600

    Paso 4.13

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const config = require("./utils/config")
 const express = require("express")
+require("express-async-errors")
 const app = express()
 const cors = require("cors")
 const blogsRouter = require("./controllers/blogs")
@@ -8,13 +9,13 @@ const logger = require("./utils/logger")
 const mongoose = require("mongoose")
 
 mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info("connected to MongoDB")
-  })
-  .catch(error => {
-    logger.error("error connecting to MongoDB:", error.message)
-  })
+    .connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info("connected to MongoDB")
+    })
+    .catch(error => {
+        logger.error("error connecting to MongoDB:", error.message)
+    })
 
 app.use(cors())
 app.use(express.static("build"))
