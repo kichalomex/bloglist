4cd5e7ec3a36ad9567df8cd7b43f9cab34412bf6
kichalomex <kichalo23con@hotmail.com>
2023-05-02 22:23:13 -0600

    Paso 4.11

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,17 +2,24 @@ const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
 
 blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs)
-  })
+    Blog.find({}).then(blogs => {
+        response.json(blogs)
+    })
 })
 
 blogsRouter.post("/", (request, response) => {
-  const blog = new Blog(request.body)
+    const body = request.body
 
-  blog.save().then(result => {
-    response.status(201).json(result)
-  })
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+    })
+
+    blog.save().then(result => {
+        response.status(201).json(result)
+    })
 })
 
 module.exports = blogsRouter
