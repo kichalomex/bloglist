700f582a8f97ddcbecf924fda6e1893c5af52aaa
kichalomex <kichalo23con@hotmail.com>
2023-05-02 21:27:34 -0600

    Paso 4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -19,12 +19,13 @@ const favoriteBlog = blogs => {
 }
 
 const mostBlogs = blogs => {
-    const x = a => {
-      return a
-    }
-  
-    return 1
-  }
+    const groupedBlogs = _.groupBy(blogs, "author")
+    const blogsAuthor = _.mapValues(groupedBlogs, "length")
+    const toArray = Object.entries(blogsAuthor)
+    const mayorPair = toArray.reduce((a, b) => (a[1] > b[1] ? a : b))
+    
+    return { author: mayorPair[0], blogs: mayorPair[1] }  
+}
 
 module.exports = {
     dummy,
